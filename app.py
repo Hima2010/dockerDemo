97159bb1b1dddf32d10aeb0ee01c7ca10f9519c2
Hima2010 <88380671+Hima2010@users.noreply.github.com>
2024-02-24 22:29:11 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+class HelloWorld:
+    def say_hello(self):
+        """Prints 'Hello, World!'"""
+        print("Hello, World!")
+
+# Create an instance of the HelloWorld class
+hello = HelloWorld()
+
+# Call the say_hello method to print the greeting
+hello.say_hello()
